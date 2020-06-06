7872af9331072c106b50fb67c6c115a254cdc08f
Rahul Shah <rshah5@ncsu.edu>
2020-06-05 21:14:53 -0400

    Add notebook-tracker

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import {
   JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  JupyterFrontEndPlugin,
 } from '@jupyterlab/application';
 
-// import { INotebookTracker } from '@jupyterlab/notebook';
+import { INotebookTracker } from '@jupyterlab/notebook';
 
 import { IMainMenu 
 } from '@jupyterlab/mainmenu'
 import { Menu } from '@lumino/widgets';
+import { CommandRegistry } from '@lumino/commands';
 
 /**
  * Initialization data for the run-tag-cells-jlab extension.
@@ -15,30 +16,49 @@ import { Menu } from '@lumino/widgets';
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'run-tag-cells-jlab',
   autoStart: true,
-  requires: [IMainMenu],
-  activate: (app: JupyterFrontEnd, mainmenu: IMainMenu) => {
-    let {commands} = app;
-    const menu = new Menu({ commands });
+  requires: [IMainMenu, INotebookTracker],
+  activate: (app: JupyterFrontEnd, mainmenu: IMainMenu, tracker: INotebookTracker) => {
     function runTagCells(args: any) {
-      console.error(`Run cells with tag ${args['tag']}`);
+      console.log(`Run cells with tag ${args['tag']}`);
     }
 
+    function updateMenu(menu: Menu, tags: string[]) {
+      console.log(`Updating menu with tags: ${tags}`);
+      menu.clearItems();
+      tags.forEach((tag: string) => {
+        if(!menu.commands.hasCommand(`run-tag-cells-${tag}`)) {
+          menu.commands.addCommand(`run-tag-cells-${tag}`, {
+            label: `Run Cell with Tag '${tag}'`,
+            execute: runTagCells
+          });
+        }
+        menu.addItem({
+          command: `run-tag-cells-${tag}`,
+          args: {tag}
+        });
+      });
+    }
+    const commands: CommandRegistry = new CommandRegistry();
+    const menu = new Menu({ commands });
     menu.title.label = 'Run Tagged Cells';
-    const tags = ['a', 'b', 'c'];
-    tags.forEach((tag: string) => {
-      commands.addCommand(`run-tag-cells-${tag}`, {
-        label: `Run Cell with Tag '${tag}'`,
-        execute: runTagCells
-      });
-      menu.addItem({
-        command: `run-tag-cells-${tag}`,
-        args: {tag}
-      });
-    });    
     mainmenu.addMenu(menu, { rank: 60 }); 
+
+    tracker.currentChanged.connect((sender, args) => {
+      const notebook = tracker.currentWidget;
+      // If notebook is added
+      if(notebook) {
+        const cells = notebook.content.model.cells;
+        let tags: string[] = [];
+        for(let i = 0; i <= cells.length; i += 1) {
+          const cell = cells.get(i);
+          if(cell) {
+            tags = tags.concat(cell.metadata.keys());
+          }
+        }
+        updateMenu(menu, tags);
+      }
+    });
   }
-};
-
-
+}
 
 export default extension;
